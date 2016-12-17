eb70aaa8d998f99670ab1ca4cf328b5ebaa8b2ca
SMARTIUM <dev@smartium.io>
2016-12-17 00:36:28 -0200

    Adicionado o visual

--- client/main.js
+++ client/main.js
@@ -3,24 +3,9 @@ var counter = 0;
 var number = new ReactiveVar();
 var arrRaffles = [];
 var raffles = new ReactiveVar([]);
-var isPlaying = new ReactiveVar(true);
-var isRunning = new ReactiveVar();
-
-Template.body.onRendered(function() {
-
-  $('#fullpage').fullpage();
-
-  let settings = 'particles.json';
-  this.autorun(() => {
-    if (particlesJS) {
-      console.log(`loading particles.js config from "${settings}"...`)
-      /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-      particlesJS.load('particles-js', settings, function () {
-        console.log('callback - particles.js config loaded');
-      });
-    }
-  });
-});
+var raffleCount = new ReactiveVar();
+// var isPlaying = new ReactiveVar(true);
+var isRunning = new ReactiveVar('');
 
 Template.body.helpers({
   number() {
@@ -42,24 +27,26 @@ Template.body.helpers({
 
 Template.body.events({
   'click #go'() {
-    if (arrRaffles.length >= 4) {
-      isPlaying.set(false);
-    }
+    // if (arrRaffles.length >= 4) {
+    //   isPlaying.set(false);
+    // }
 
-    isRunning.set('none');
-    counter = 0;
-    var myInterval = Meteor.setInterval(function(){
-      counter ++
-      drawn = Math.round((Math.random() * 1000) + 1)
-      number.set(drawn)
-      if (counter == 300) {
-        Meteor.clearInterval(myInterval);
-        console.log(number.get());
-        arrRaffles.push(number.get());
-        raffles.set(arrRaffles);
-        isRunning.set('');
-      }
-    }, 10);
+    if (isRunning.get() == '') {
+      isRunning.set('disabled');
+      counter = 0;
+      var myInterval = Meteor.setInterval(function(){
+        counter ++
+        drawn = Math.round((Math.random() * 1000) + 1)
+        number.set(drawn)
+        if (counter == 3) {
+          Meteor.clearInterval(myInterval);
+          console.log(number.get());
+          arrRaffles.push(number.get());
+          raffles.set(arrRaffles);
+          isRunning.set('');
+        }
+      }, 10);
+    }
   },
 
   'click #new'() {
